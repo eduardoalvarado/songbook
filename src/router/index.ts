1f26e23c958a86e12b4941123f90d5fe62d6af5a
Oscar Alvarado <oalvarado@zetech.com.ar>
2021-04-23 10:33:43 -0500

    test PAT for git repository

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
-    component:() => import(/*home*/ '../views/Home.vue')
+    component:() => import(/*home name*/ '../views/Home.vue')
   },
   {
     path: '/about',
