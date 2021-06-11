cd812825655a35f11847ed9d282513a2d75de2c4
Oscar Alvarado <oalvarado@zetech.com.ar>
2021-06-11 01:17:32 -0500

    refactor server.js and move to root directory

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const path = require('path')
 
 const app = express()
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
-app.get(/.*/, function(req, res) {
-    res.sendFile(path.join(__dirname, '/dist/index.html'))
-})
+// app.get(/.*/, function(req, res) {
+//     res.sendFile(path.join(__dirname, '/dist/index.html'))
+// })
 const port = process.env.PORT || 8080
 app.listen(port)
 console.log('Listening on port: ' + port)
