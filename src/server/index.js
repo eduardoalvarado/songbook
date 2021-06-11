a5a394ad7272f6239ba46094733ec2a4bf9504d8
Oscar Alvarado <oalvarado@zetech.com.ar>
2021-06-11 00:54:00 -0500

    change import for required

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,6 @@
-import express from 'express'
-import serveStatic from 'serve-static'
-import path from 'path'
+const express = require('express')
+const serveStatic =require('serve-static')
+const path = require('path')
 
 const app = express()
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
