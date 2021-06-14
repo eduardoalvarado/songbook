7848a268736b5570f715dd4badeb7f71a339d11b
Oscar Alvarado <oalvarado@zetech.com.ar>
2021-06-14 16:09:09 -0500

    remove fontawesome

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/tailwind.css'
-import { library } from '@fortawesome/fontawesome-svg-core'
+/*import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { fas } from '@fortawesome/free-solid-svg-icons'
-library.add(fas)
+library.add(fas)*/
 
 createApp(App)
-  .component('font-awesome-icon', FontAwesomeIcon)
+  /*.component('font-awesome-icon', FontAwesomeIcon)*/
   .use(store)
   .use(router)
   .mount('#app')
